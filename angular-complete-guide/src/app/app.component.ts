c1707c6c81eb05915964c2d71b6a7c0d3ead23c2
Norton T GUENO <nortontgueno@hotmail.com>
2018-12-29 11:42:43 -0200

    String interpolation and property binding

--- angular-complete-guide/src/app/app.component.ts
+++ angular-complete-guide/src/app/app.component.ts
@@ -5,9 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   // styleUrls: ['./app.component.css']
   styles: [`
-    h3 {
-      color: dodgerblue;
-    }
+        h3 {
+          color: dodgerblue;
+        }
   `]
 })
 export class AppComponent {
--- angular-complete-guide/src/app/server/server.component.ts
+++ angular-complete-guide/src/app/server/server.component.ts
@@ -5,5 +5,13 @@ import {Component} from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverId = 10;
+    serverStatus = 'offline';
 
+    getServerStatus() {
+      return this.serverStatus;
+    }
+
+  // serverId: number = 10;
+  // serverStatus: string = 'offline';
 }
