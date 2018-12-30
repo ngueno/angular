18e0a4c978dddefabac52ac5f57b03b3ac0a3258
Norton T GUENO <nortontgueno@hotmail.com>
2018-12-29 22:20:46 -0200

    ngStyle and ngClass directives

--- angular-complete-guide/src/app/server/server.component.ts
+++ angular-complete-guide/src/app/server/server.component.ts
@@ -2,11 +2,20 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+  `]
 })
 export class ServerComponent {
     serverId = 10;
     serverStatus = 'offline';
+
+    constructor() {
+      this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
 
     getServerStatus() {
       return this.serverStatus;
@@ -14,4 +23,7 @@ export class ServerComponent {
 
   // serverId: number = 10;
   // serverStatus: string = 'offline';
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
