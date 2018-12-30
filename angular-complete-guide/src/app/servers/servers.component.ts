3276070eb900b175a8dcf00799ad15cccfafe98b
Norton T GUENO <nortontgueno@hotmail.com>
2018-12-29 22:45:19 -0200

    Challenge with ngFor, ngClass and ngStyle

--- angular-complete-guide/src/app/servers/servers.component.ts
+++ angular-complete-guide/src/app/servers/servers.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
+  styles: [`
+    .attentionLog {
+      color: white;
+      background-color: blue;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
@@ -11,9 +17,12 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = '';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
 
   username = '';
-  allowResetUsername = false;
+  showDetails = false;
+  clicks = [];
+  clickCounter = 0;
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +35,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
 
@@ -36,4 +46,9 @@ export class ServersComponent implements OnInit {
   resetUsername() {
     this.username = '';
   }
+
+  onDisplayDetails() {
+    this.clicks.push(++this.clickCounter);
+  }
+
 }
